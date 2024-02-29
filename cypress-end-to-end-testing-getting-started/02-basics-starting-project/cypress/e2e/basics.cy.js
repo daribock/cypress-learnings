f3d8a72c384a38bc706a221c7b12faaaa4bd6ee0
Darius Kletter <daribock@gmail.com>
2024-02-29 15:34:11 +0100

    🧪 Add cypress tests

--- cypress-end-to-end-testing-getting-started/02-basics-starting-project/cypress/e2e/basics.cy.js
+++ cypress-end-to-end-testing-getting-started/02-basics-starting-project/cypress/e2e/basics.cy.js
@@ -6,7 +6,7 @@ describe('template spec', () => {
     cy.get('.main-header img');
   });
 
-  it('should display teh page title', () => {
+  it('should display the page title', () => {
     cy.visit('http://localhost:5173/');
     cy.get('h1').should('have.length', 1);
     cy.get('h1').contains('My Cypress Course Tasks');
--- cypress-end-to-end-testing-getting-started/02-basics-starting-project/cypress/e2e/tasks.cy.js
+++ cypress-end-to-end-testing-getting-started/02-basics-starting-project/cypress/e2e/tasks.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('tasks management', () => {
-  it('should open and close the new taks modal', () => {
+  it('should open and close the new task modal', () => {
     cy.visit('http://localhost:5173/');
     cy.get('button').contains('Add Task').click();
     cy.get('.backdrop').click({ force: true });
